f4c826b1a55992c4fb4b02ffc34c9f11b355b140
dididy <dev.yongjaelee@gmail.com>
2021-02-04 15:33:06 +0900

    chore(env): 평소에 사용하던 프로젝트 보일러플레이트 환경 적용
    - 초기 파일들 삭제
    - lint 적용
    - styled-components global styles, theme 적용
    - craco를 이용한 alias 적용
    - 폴더구조 반영

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import { Switch, Route } from 'react-router-dom';
+
+import P from '@pages';
+// import DetailPage from '@/pages/DetailPage';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/" component={P.LandingPage} />
+      {/* <Route exact path="/detail:id" component={DetailPage} /> */}
+    </Switch>
   );
 }
-
-export default App;
---
+++ src/components/Button/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Button() {
+  return (
+    <button type="button" oncClick={() => {}}>
+      Test Button
+    </button>
+  );
+}
