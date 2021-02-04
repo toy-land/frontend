e034167957187fa244be5e0769f2e9b9e2c3bdd7
dididy <dev.yongjaelee@gmail.com>
2021-02-04 17:51:37 +0900

    feat(component): Tag 아토믹 컴포넌트 제작 완료
    - 디자인 나오기 전 GitHub 태그 형태 카피 -> 추후에 사용
    - polished로 darken을 이용해 hover 컬러 값 변경
    - 함수 실행 가능하도록 함

---
+++ src/atoms/Tag/Tag.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+import colorType from '@constants/colorType';
+
+const Wrapper = styled.div`
+  width: fit-content;
+  height: 26px;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  color: ${({ fontColor }) => fontColor};
+  display: fles;
+  justify-content: center;
+  align-items: center;
+  margin: 5px;
+  border-radius: 13px;
+  &:hover {
+    background-color: ${({ backgroundColor }) => darken(0.04, backgroundColor)};
+  }
+`;
+
+const WrapText = styled.div`
+  padding-left: 10px;
+<<<<<<< HEAD
+  ${({ hasX }) => (hasX === true ? 'padding-right: 5px;' : 'padding-right: 10px;')}
+=======
+  ${({ hasX }) => (hasX ? 'padding-right: 5px;' : 'padding-right: 10px;')}
+>>>>>>> b423f13 (chore(env): styled-components GlobalStyles 적용)
+  font-size: 12px;
+  font-weight: bold;
+`;
+
+const WrapCancel = styled.div`
+  display: fles;
+  justify-content: center;
+  align-items: center;
+  width: 26px;
+  height: 26px;
+  border-radius: 13px;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  cursor: pointer;
+  font-size: 12px;
+  &:hover {
+    background: ${({ backgroundColor }) => darken(0.04, backgroundColor)};
+  }
+`;
+
+export default function Tag({
+  text, func, type = 0,
+}) {
+  return (
+    <Wrapper
+      fontColor={colorType[type].fontColor}
+      backgroundColor={colorType[type].background}
+    >
+      <WrapText hasX={func}>{text}</WrapText>
+      {func && (
+        <WrapCancel backgroundColor={colorType[type].background} onClick={func}>
+          ✕
+        </WrapCancel>
+      )}
+    </Wrapper>
+  );
+}
