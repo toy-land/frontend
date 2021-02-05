4e45d361f0710b28ec3a8484c8ac4299cd44c97d
OhSeungYoung <osy0056@khu.ac.kr>
2021-02-06 05:37:44 +0900

    feat: LandingPage 수정, 표지 추가

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,10 +1,46 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
-import styled from 'styled-components';
+import React, { useEffect, useState } from 'react';
+import styled, { keyframes } from 'styled-components';
+import LandingWallpaper from '@assets/images/LandingWallpaper.png';
 
 import C from '@components';
 
+const slideUp = keyframes`
+  from {
+    transform: translateY(200px);
+  }
+  to {
+    transform: translateY(0px);
+  }
+`;
+
+const slideDown = keyframes`
+  from {
+    transform: translateY(-70px);
+  }
+  to {
+    transform: translateY(0px);
+  }
+`;
+
+const WallPaper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  animation-duration: 0.3s;
+  animation-timing-function: ease-in-out;
+  animation-name: ${slideDown};
+  animation-fill-mode: forwards;
+  display: flex;
+  justify-content: center;
+  img {
+    height: 85vh;
+  }
+`;
+
 const Wrapper = styled.div`
+  width: 100vw;
+  height: 110vh;
   display: flex;
   align-items: flex-start;
   justify-content: center;
@@ -21,16 +57,54 @@ const WrapContainer = styled.div`
   justify-content: center;
 `;
 
+const Main = styled.div`
+  width: inherit;
+  height: inherit;
+  animation-duration: 0.3s;
+  animation-timing-function: ease-in-out;
+  animation-name: ${slideUp};
+  animation-fill-mode: forwards;
+
+`;
+
 export default function LandingPage() {
+  const [isScroll, setIsScroll] = useState(false);
+
+  const onScroll = () => {
+    if (document.documentElement.scrollTop > 3) {
+      setIsScroll(true);
+    } else {
+      setIsScroll(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    // return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
   return (
     <>
       <Wrapper>
         <WrapContainer>
           <C.Header />
-          <C.MainView />
-          <C.SideIconBox />
+          {isScroll
+            ? (
+              <Main>
+                <C.MainView />
+                <C.SideIconBox />
+              </Main>
+            )
+            : (
+              <>
+                <WallPaper active>
+                  <img src={LandingWallpaper} />
+                </WallPaper>
+              </>
+            )}
         </WrapContainer>
       </Wrapper>
+
     </>
   );
 }
