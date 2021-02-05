ba2c8df9250e87aa40dd347814e0eb8da42bbe3b
dididy <dev.yongjaelee@gmail.com>
2021-02-06 00:05:03 +0900

    feat: InputRepoPage 제작 및 라우팅 처리 완료
    - 분기는 CreateToyPage에서 발생함

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import DetailPage from './DetailPage';
 import LandingPage from './LandingPage';
 import CreateToyPage from './CreateToyPage';
 import DeletePage from './DeletePage';
+import InputRepoPage from './InputRepoPage';
 
 export default {
   LandingPage,
   DetailPage,
   CreateToyPage,
   DeletePage,
+  InputRepoPage,
 };
