085424db3fea7e41967337069d1f52e09c0d1c3d
OhSeungYoung <osy0056@khu.ac.kr>
2021-02-05 20:31:06 +0900

    fix: CI issue 해결

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,11 @@
 import CardBox from './CardBox';
 import MainView from './MainView';
 import Header from './Header';
-import CreateButton from './CreateButton';
 import SideIconBox from './SideIconBox';
 
 export default {
   CardBox,
   MainView,
   Header,
-  CreateButton,
   SideIconBox,
 };
