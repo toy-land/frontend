f1f02fb5f47b4ba134dbbdffd5afc93cc3aa2fee
dididy <dev.yongjaelee@gmail.com>
2021-02-05 23:54:44 +0900

    feat(thunk): organization 및 category 목록을 가져오는 리듀서 추가
    - 이걸 바탕으로 드랍다운에 적용해야 함

--- src/modules/getSkills/actions.js
+++ src/modules/getSkills/actions.js
@@ -1,3 +1,11 @@
 export const GET_SKILLS = 'getSkills/GET_SKILLS';
 export const GET_SKILLS_SUCCESS = 'getSkills/GET_SKILLS_SUCCESS';
 export const GET_SKILLS_FAIL = 'getSkills/GET_SKILLS_FAIL';
+
+export const GET_ORGANIZATIONS = 'getOrganizations/GET_ORGANIZATIONS';
+export const GET_ORGANIZATIONS_SUCCESS = 'getOrganizations/GET_ORGANIZATIONS_SUCCESS';
+export const GET_ORGANIZATIONS_FAIL = 'getOrganizations/GET_ORGANIZATIONS_FAIL';
+
+export const GET_CATEGORIES = 'getCategories/GET_CATEGORIES';
+export const GET_CATEGORIES_SUCCESS = 'getCategories/GET_CATEGORIES_SUCCESS';
+export const GET_CATEGORIES_FAIL = 'getCategories/GET_CATEGORIES_FAIL';
