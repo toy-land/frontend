8f5b77f14206b08396cea8f759490638776fd7a3
OhSeungYoung <osy0056@khu.ac.kr>
2021-02-06 09:31:29 +0900

    fix: 랜딩 스크롤 될 때 get request 하는 버그 해결

--- src/modules/getToy/actions.js
+++ src/modules/getToy/actions.js
@@ -4,6 +4,11 @@ export const GET_TOY_SUCCESS = 'getToy/GET_TOY_SUCCESS';
 export const GET_TOY_FAIL = 'getToy/GET_TOY_FAIL';
 
 // 여러개 조회하기
+export const INITIALIZE_TOYS = 'getToy/INITIALIZE_TOYS';
 export const GET_TOYS = 'getToy/GET_TOYS'; // 요청 시작
 export const GET_TOYS_SUCCESS = 'getToy/GET_TOYS_SUCCESS'; // 요청 성공
 export const GET_TOYS_FAIL = 'getToy/GET_TOYS_FAIL'; // 요청 실패
+
+export const initializeToys = () => ({
+  type: INITIALIZE_TOYS,
+});
