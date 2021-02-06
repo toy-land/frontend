74ef46f9fedcbc475013dc75ef77223258c46423
OhSeungYoung <osy0056@khu.ac.kr>
2021-02-06 11:11:18 +0900

    fix: 삭제 완료 후 메인 페이지 해결

--- src/modules/removeToy/thunk.js
+++ src/modules/removeToy/thunk.js
@@ -1,8 +1,16 @@
 import * as api from '@utils/api';
-import { createPromiseThunk } from '@utils/asyncUtils';
+import { useHistory } from 'react-router-dom';
 import * as actions from './actions';
 
-export const removeToyThunk = createPromiseThunk(
-  actions.REMOVE_TOY,
-  api.deleteToyApi,
-);
+export const removeToyThunk = (param, setRemoveToggle) => async (dispatch) => {
+  dispatch({ type: actions.REMOVE_TOY }); // 요청이 시작됨
+  try {
+    const payload = await api.deleteToyApi(param);
+    alert('삭제 성공!');
+    setRemoveToggle(false);
+    dispatch({ type: actions.REMOVE_TOY_SUCCESS, payload: payload.data }); // 성공
+  } catch (e) {
+    alert(e);
+    dispatch({ type: actions.REMOVE_TOY_FAIL, payload: e.message }); // 실패
+  }
+};
