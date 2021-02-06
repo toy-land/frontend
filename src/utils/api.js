13340b6081d6cabd0d08744ff509c3057a2baa7d
dididy <dev.yongjaelee@gmail.com>
2021-02-06 12:09:35 +0900

    fix: 토큰

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,21 +2,21 @@ import axios from 'axios';
 
 const apiUrl = 'http://49.50.175.141:8080/api';
 
-export const getToyApi = (id) => axios.get(`${apiUrl}/toys/${id}`);
-
-export const getToysApi = (page = 0) => axios.get(`${apiUrl}/toys?page=${page}`);
-
-export const getSkillsApi = () => axios.get(`${apiUrl}/skills`);
-
-export const getOrgnizationsApi = () => axios.get(`${apiUrl}/organizations`);
-
-export const getCategoriesApi = () => axios.get(`${apiUrl}/categories`);
-
 const config = {
-  headers: { Authorization: 'token 226bece4c9c4ff2e0ec6228db5ee4a2b6e87bc9a' },
+  headers: { Authorization: `token ${process.env.FRONT_API_TOKEN}` },
 };
 
-export const postToyApi = (data) => axios.post(`${apiUrl}/toys`, data, config);
+export const getToyApi = (id) => axios.get(`${apiUrl}/toys/${id}`, config);
+
+export const getToysApi = (page = 0) => axios.get(`${apiUrl}/toys?page=${page}`, config);
+
+export const getSkillsApi = () => axios.get(`${apiUrl}/skills`, config);
+
+export const getOrgnizationsApi = () => axios.get(`${apiUrl}/organizations`, config);
+
+export const getCategoriesApi = () => axios.get(`${apiUrl}/categories`, config);
+
+export const postToyApi = (data) => axios.post(`${apiUrl}/toys`, data);
 
 export const putToyApi = ({ id, passwd }) => axios.put(`${apiUrl}/toys/${id}`, { passwd });
 
