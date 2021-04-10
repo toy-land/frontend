a5a18227bce5db7a153c8e97fea86c5941d124b0
OhSeungYoung <osy0056@khu.ac.kr>
2021-04-11 01:47:20 +0900

    fix: api 호출 함수 오타 수정

--- src/utils/optionApi.js
+++ src/utils/optionApi.js
@@ -4,6 +4,6 @@ const apiUrl = 'http://49.50.175.141:8080/api';
 
 export const getSkillsApi = () => axios.get(`${apiUrl}/skills`);
 
-export const getOrgnizationsApi = () => axios.get(`${apiUrl}/organizations`);
+export const getOrganizationsApi = () => axios.get(`${apiUrl}/organizations`);
 
 export const getCategoriesApi = () => axios.get(`${apiUrl}/categories`);
