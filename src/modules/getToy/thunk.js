0ee06f38fce258f12ac29f09a5c0fe8b96f2dd25
OhSeungYoung <osy0056@khu.ac.kr>
2021-05-17 01:35:30 +0900

    feat: getMoreToys Redux Action 추가

--- src/modules/getToy/thunk.js
+++ src/modules/getToy/thunk.js
@@ -1,9 +1,13 @@
 import * as toyApi from '@utils/toyApi';
 import { createPromiseThunk } from '@utils/asyncUtils';
-import { getToyAsyncAction, getToysAsyncAction } from './actions';
+import { getMoreToysAsyncAction, getToyAsyncAction, getToysAsyncAction } from './actions';
 
 export const getToyThunk = createPromiseThunk(getToyAsyncAction, toyApi.getToyApi);
 export const getToysThunk = createPromiseThunk(
   getToysAsyncAction,
+  toyApi.getToysApi,
+);
+export const getMoreToysThunk = createPromiseThunk(
+  getMoreToysAsyncAction,
   toyApi.getToysApi,
 );
