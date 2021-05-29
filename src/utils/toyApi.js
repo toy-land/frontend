f8687aa64aeefa5606fbbba3630d178d9022c2ac
OhSeungYoung <osy0056@khu.ac.kr>
2021-05-30 04:45:07 +0900

    feat: getMoreToysApi 추가
    스크롤을 내려서 Toy를 요청할 때 예외처리가 용이하도록 따로 api를 만들었습니다.

--- src/utils/toyApi.js
+++ src/utils/toyApi.js
@@ -1,9 +1,12 @@
+import { toyNumPerPage } from '@constants/toyNumPerPage';
 import { apiUrl } from '@constants/url';
 import axios from 'axios';
 
 export const getToyApi = (id) => axios.get(`${apiUrl}/toys/${id}`);
 
-export const getToysApi = (page = 0) => axios.get(`${apiUrl}/toys?page=${page}`);
+export const getToysApi = (page = 0) => axios.get(`${apiUrl}/toys?page=${page}&size=${toyNumPerPage}`);
+
+export const getMoreToysApi = (page) => axios.get(`${apiUrl}/toys?page=${page}&size=${toyNumPerPage}`);
 
 export const postToyApi = (data) => axios.post(`${apiUrl}/toys`, data);
 
